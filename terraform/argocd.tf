4266ecd09231dbae098ff8d66a2abdf3cc9acf5f
westelh <westelh@gmail.com>
2023-12-21 23:54:03 +0900

    Update terraform codes

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,10 +1,22 @@
 data "vault_policy_document" "argocd" {
-  path         = "kv/data/argocd/*"
-  capabilities = ["read"]
-  description  = "ArgoCD can insert secrets into the configuration."
+  rule {
+    path         = "kv/data/argocd/*"
+    capabilities = ["read"]
+    description  = "ArgoCD can insert secrets into the configuration."
+  }
 }
 
 resource "vault_policy" "argocd" {
   name   = "argocd"
   policy = data.vault_policy_document.argocd.hcl
+}
+
+resource "vault_kubernetes_auth_backend_role" "argocd" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "argocd"
+  bound_service_account_names      = ["argocd-repo-server"]
+  bound_service_account_namespaces = ["argocd"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.argocd.name]
+  audience                         = "vault"
 }
--- terraform/vault-issuer.tf
+++ terraform/vault-issuer.tf
@@ -1,5 +1,22 @@
 data "vault_policy_document" "vault_issuer" {
-  path         = "pki_int1/sign/westelh-dev"
-  capabilities = ["create", "update"]
-  description  = "Vault Issuer can sign certificates for westelh.dev domain."
+  rule {
+    path         = "pki_int1/sign/westelh-dev"
+    capabilities = ["create", "update"]
+    description  = "Vault Issuer can sign certificates for westelh.dev domain."
+  }
+}
+
+resource "vault_policy" "vault_issuer" {
+  name   = "vault-issuer"
+  policy = data.vault_policy_document.vault_issuer.hcl
+}
+
+resource "vault_kubernetes_auth_backend_role" "vault_issuer" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "vault-issuer"
+  bound_service_account_names      = ["vault-issuer"]
+  bound_service_account_namespaces = ["*"]
+  token_ttl                        = 3600
+  token_policies                   = [vault_policy.vault_issuer.name]
+  audience                         = "vault"
 }
